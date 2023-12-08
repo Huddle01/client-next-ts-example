517a6c659ee9d6183e75576b47ea9f0b4568309f
Mizan Ali <mizanalip786@gmail.com>
2023-12-08 14:26:58 +0530

    code cleanup and formatting

--- src/component/RemotePeer/RemotePeer.tsx
+++ src/component/RemotePeer/RemotePeer.tsx
@@ -7,28 +7,13 @@ type Props = {
 
 const RemotePeer = ({ peerId }: Props) => {
   const { stream, state } = useRemoteVideo({ peerId });
+
   const vidRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
-    console.log({ RemotePeer: peerId });
-
     if (stream && vidRef.current && state === "playable") {
-      console.log("here 1");
-
       vidRef.current.srcObject = stream;
-
       vidRef.current?.play();
-      // vidRef.current.onloadedmetadata = async () => {
-      //   try {
-      //     console.log("here 2");
-      //   } catch (error) {
-      //     console.error(error);
-      //   }
-      // };
-
-      vidRef.current.onerror = () => {
-        console.error("videoCard() | Error is hapenning...");
-      };
     }
   }, [stream, state]);
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,8 @@
-import { Inter } from "next/font/google";
-import {
-  useLocalPeer,
-  useLocalVideo,
-  usePeerIds,
-  useRoom,
-} from "@huddle01/react/hooks";
-import { useEffect, useRef } from "react";
 import RemotePeer from "@/component/RemotePeer/RemotePeer";
+import { useLocalVideo, usePeerIds, useRoom } from "@huddle01/react/hooks";
 import { AccessToken, Role } from "@huddle01/server-sdk/auth";
+import { Inter } from "next/font/google";
+import { useEffect, useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +19,6 @@ export default function Home({ token }: Props) {
       console.log("onPeerJoin", peer);
     },
   });
-
   const { enableVideo, isVideoOn, stream } = useLocalVideo();
   const { peerIds } = usePeerIds();
 
@@ -35,8 +29,6 @@ export default function Home({ token }: Props) {
       videoRef.current.srcObject = stream;
     }
   }, [stream]);
-
-  console.log({ peerIds });
 
   return (
     <main
@@ -71,15 +63,6 @@ export default function Home({ token }: Props) {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        {/* <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        /> */}
-
         {isVideoOn && (
           <video
             ref={videoRef}
