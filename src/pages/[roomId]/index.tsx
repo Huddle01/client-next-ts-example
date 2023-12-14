381fb88375bae062b708d6a007e7fea6ccbe9016
vrajdesai78 <vrajdesai78@gmail.com>
2023-12-14 19:01:11 +0530

    Updated UI

--- src/pages/[roomId]/index.tsx
+++ src/pages/[roomId]/index.tsx
@@ -10,7 +10,6 @@ import {
   useRoom,
 } from '@huddle01/react/hooks';
 import { AccessToken, Role } from '@huddle01/server-sdk/auth';
-import { Recorder } from '@huddle01/server-sdk/recorder';
 import { Inter } from 'next/font/google';
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
@@ -148,22 +147,24 @@ export default function Home({ token }: Props) {
           <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
             <div className="relative flex gap-2">
               {isVideoOn && (
-                <div>
+                <div className="w-1/2 mx-auto border-2 rounded-xl border-blue-400">
                   <video
                     ref={videoRef}
-                    className="w-1/2 mx-auto border-2 rounded-xl border-blue-400"
+                    className="aspect-video rounded-xl"
                     autoPlay
                     muted
                   />
                 </div>
               )}
               {shareStream && (
-                <video
-                  ref={screenRef}
-                  className="w-1/2 mx-auto border-2 rounded-xl border-blue-400"
-                  autoPlay
-                  muted
-                />
+                <div className="w-1/2 mx-auto border-2 rounded-xl border-blue-400">
+                  <video
+                    ref={screenRef}
+                    className="aspect-video rounded-xl"
+                    autoPlay
+                    muted
+                  />
+                </div>
               )}
             </div>
           </div>
@@ -202,6 +203,8 @@ export const getServerSideProps = async (ctx: any) => {
   });
 
   const token = await accessToken.toJwt();
+
+  console.log(token);
 
   return {
     props: { token },
